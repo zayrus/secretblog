f955f37300947ded7efe4f758642fba86b998f50
Ernesto venencio <zayrus@gmail.com>
2017-05-26 17:41:25 -0300

    feat(improve model): complete article model with media comments and tags

--- api/api/models/article.js
+++ api/api/models/article.js
@@ -2,8 +2,33 @@ var mongoose  = require('mongoose')
 var Schema    = mongoose.Schema
 
 var ArticlesSchema = new Schema({
+  writeDate: {
+    type: Date,
+    default: Date.now
+  },
   title: String,
-  body: String
+  body: String,
+  images: [{
+    url: String
+  }],
+  videos: [{
+    url: String
+  }],
+  tags: [{
+    name: String
+  }],
+  comments: [{
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    name: String,
+    text: String
+  }],
+  likes: Number,
+  relatedPosts: [{
+    id: String
+  }]
 })
 
 const ArticleModel = mongoose.model('Article', ArticlesSchema)
